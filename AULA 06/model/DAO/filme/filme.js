47673cc32aca48f5d007df502ecd120a9d76032b
STHEFANY DA SILVA CARMO CORREIA <25203734@CFP127ALUNO.LOCAL>
2026-04-15 11:30:07 -0300

    Unindo Bancos de Dados com Back-end

---
+++ AULA 06/model/DAO/filme/filme.js
@@ -0,0 +1,105 @@
+/*********************************************************************************************************************************
+ * Objetivo: Arquivo responsável pelo CRUD no Banco de Dados MySQL na tabela
+ *      Filme
+ * Data: 15/04/2026
+ * Autor: Marcel
+ * Varsão: 1.0
+ * 
+ *                                              Instalações:
+ * 
+ * npm install express --save
+ * npm install cors --save
+ * 
+ * npm install knex --save : biblioteca p/ se conectar com o banco de dados, existem várias dependencias e ele é uma delas.
+ * outras bibliotecas que se conectam com o BD: 
+ * 
+ * Sequelize (mais básico e antigo, prof recomendou evitar o uso)
+ * Prisma (É bom, porém anda instável)
+ * Knex
+ * Tudo depende do BD que vc está utilizando.
+ * *******************************************************************************************************************************/
+
+//                                              Basicamente ctrl c ctrl v
+
+ //Import da biblioteca para gereciar o bando de dados Mysql no node.JS
+const knex = require('knex')
+
+//Import do arquivo de configuração para conexão com o BD Mysql
+const knexConfig = require('../../database_config_knex/knexFile.js') // ../para sair da pasta
+
+//Criar a conexão com o BD Mysl
+const knexConex = knex(knexConfig.development)
+
+
+ //                                                Funções do CRUD para a tabela filme (a base p/ criar qualquer tabela)
+
+//Função para inserir dados na tabela de filme
+const insertFilme = async function(filme) { 
+
+    //nesse caso o parametro será um json (filme)
+    //copiei o código do MySQL
+    //Do jeito que vc escreveu os atributos no BD, devo escrever no json (filme), 
+    //se não a minha controller não vai aceitar e devolverá um 404, já que quem 'manda' no BD é o back. 
+    //Organize tudo MUITO bem!
+
+    let sql = `insert into tbl_filme (
+					nome, 
+					data_lacamento, 
+                    duracao, 
+                    sinopse, 
+                    avaliacao, 
+                    valor, 
+                    capa
+                        )
+                values (
+			        '${filme.nome}', 
+                    '${filme.data_lancamento}', 
+                    '${filme.duracao}',
+                    '${filme.sinopse}',
+                    '${filme.avaliacao}',
+                    '${filme.valor}',
+                    '${filme.capa}'
+        );`
+
+    //Pego meu parametro (filme) . e coloco os atributos da tabela, EXATAMENTE como foram criados e dessa forma tudo fica mais claro e organizado.
+
+    //Executar o ScriptSQL no banco de dados
+    let result = await knexConex.raw(sql) //precisa colocar o await para que o BD faça a analise e consiga fazer devolutiva, se não ele passa direto pelo js
+
+    if(result){
+        return true
+    }else
+        return false
+
+        //A controller é quem trata esse true ou false, retornando alguma numeração que sign alguma resposta
+
+        
+}
+
+//Função para atualizar um filme existente na tabela
+const updateFilme = async function(filme) {
+
+}
+
+//Função para retornar todos os dados da tabela de filme
+const selectAllFilme = async function (filme) { //selectAll (trás tudo)
+    
+}
+
+//Função para retornar os dados do filme filtrando pelo ID
+const selectByIdFilme = async function (id) {
+    
+}
+
+//Função para excluir um filme pelo ID
+const deleteFilme = async function (id) {
+    
+}
+
+module.exports = {
+    insertFilme,
+    updateFilme,
+    selectAllFilme,
+    selectByIdFilme,
+    deleteFilme
+}
---
+++ AULA 06/model/database/scriptSQL_filmes.sql
@@ -0,0 +1,50 @@
+#Cria o database do projeto de filmes
+create database db_filmes_20261_a;
+
+#Ativa o uso do database de filmes
+use db_filmes_20261_a;
+
+#Cria a tabela de filme
+create table tbl_filme (
+id 				int not null primary key auto_increment,
+nome 			varchar(80) not null,
+data_lacamento 	date not null,
+duracao 		time not null,
+sinopse			text not null,
+avaliacao 		decimal(3,2) default null,
+valor 			decimal(5,2) not null default 0,
+capa 			varchar(255)
+);
+
+show tables;
+
+#Inserir dados
+insert into tbl_filme (
+					nome, 
+					data_lacamento, 
+                    duracao, 
+                    sinopse, 
+                    avaliacao, 
+                    valor, 
+                    capa
+                    )
+values (
+			'Super Mario Galaxy: O Filme', 
+			'2026-04-02', 
+			'01:39:00',
+			'Uma nova aventura leva Mario a enfrentar um inédito e ameaçador super vilão. Em Super Mario Galaxy: 
+			O Filme, o bigodudo encanador italiano e seus aliados embarcam numa aventura galáctica repleta de ação
+			e momentos emocionantes depois de salvar o Reino dos Cogumelos.',
+			'3',
+			'50.70',
+			'https://br.web.img3.acsta.net/c_310_420/img/5b/ea/5bea1aeac3323aeaaf82449a34fafbbf.jpg'
+       );					
+								#2 Regras sobre values:
+			#colocar na mesma ordem dos atributos. Para evitar erros seguir a ordem da criação da tbl
+			#todos os values deve ser colado entre ''(simples) menos do tipo INT
+
+#Seleciona todas as colunas da tbl_filme, trás todos os dados da tabela
+select * from tbl_filme;
+
+
+
